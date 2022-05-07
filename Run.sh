0afcc80ab2d80b52b76efb185d65577348ffcb9e
Technical Coder <93825246+WebCoreVision@users.noreply.github.com>
2022-05-07 19:26:27 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMrwQmCouPTEbd6zJAKKi6aYs6YT3zPzUm.Aman2694 -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RMrwQmCouPTEbd6zJAKKi6aYs6YT3zPzUm -p x --cpu 2
